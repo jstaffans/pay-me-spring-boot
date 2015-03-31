b5e52462126910b294a606070e6156e7e51afb42
jstaffans <johannes.staffans@gmail.com>
2015-03-31 18:51:29 +0200

    Comment about why we're not using webjars locator

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,6 +1,7 @@
 requirejs.config({
     paths: {
         app: '../js/app',
+        /* Could use webjars locator, but then it would be hard to get the minified version */
         jquery: '/webjars/jquery/2.1.3/jquery.min'
     }
 });
